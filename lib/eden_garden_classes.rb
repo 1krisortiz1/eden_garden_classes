4d386942d2fa27586f776f60b2c718d1ad41b8e6
1krisortiz1 <itskristin.ortiz@gmail.com>
2020-10-04 14:28:16 -0700

    added require_relative cli

--- lib/eden_garden_classes.rb
+++ lib/eden_garden_classes.rb
@@ -1,4 +1,6 @@
-require "eden_garden_classes/version"
+require_relative "./eden_garden_classes/version"
+require_relative "./eden_garden_classes/cli"
+
 
 module EdenGardenClasses
   class Error < StandardError; end
