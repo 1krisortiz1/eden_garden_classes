57f3f42f96661722301b2efa2cf95bd23576061a
1krisortiz1 <itskristin.ortiz@gmail.com>
2020-10-04 15:56:51 -0700

    added categories scraping and output

--- lib/eden_garden_classes/cli.rb
+++ lib/eden_garden_classes/cli.rb
@@ -1,7 +1,21 @@
 class EdenGardenClasses::CLI
   def call
     puts "Welcome to Eden Classes!"
-    binding.pry
+    get_categories
+    get_user_category
+    # get_events_for(category)
+    # list_events
   end
 
+    def get_categories
+      # to be scraped instead
+      @categories = ['Gardening', 'Communities', 'Green Living', 'Play and Create', 'Learn at Home']
+    end
+
+    def get_user_category
+      # list categories
+      @categories.each.with_index() { | category, index| 
+        puts "#{index}. #{category}"
+      }
+    end
 end
