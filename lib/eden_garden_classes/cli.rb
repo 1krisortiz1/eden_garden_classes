1b4aae90546afb262fcbb32e15ce5165c739801b
1krisortiz1 <itskristin.ortiz@gmail.com>
2020-10-04 17:11:41 -0700

    added show_events_for method

--- lib/eden_garden_classes/cli.rb
+++ lib/eden_garden_classes/cli.rb
@@ -8,27 +8,33 @@ class EdenGardenClasses::CLI
     # list_events
   end
 
-    def get_categories
-      # to be scraped instead
-      @categories = ['Gardening', 'Communities', 'Green Living', 'Play and Create', 'Learn at Home']
-    end
+  def get_categories
+    # to be scraped instead
+    @categories = ['Gardening', 'Communities', 'Green Living', 'Play and Create', 'Learn at Home']
+  end
 
-    def list_categories
-      # list categories
-      puts "Choose a category to see classes."
-      @categories.each.with_index(1) { | category, index| 
-        puts "#{index}. #{category}"
-      }
-    end
+  def list_categories
+    # list categories
+    puts "Choose a category to see classes."
+    @categories.each.with_index(1) { | category, index| puts "#{index}. #{category}" }
+  end
 
-    def get_user_category
-      chosen_category = gets.strip
-      binding.pry
-      #if valid_input(chosen_category, @categories)
-      #end
-    end
+  def get_user_category
+    chosen_category = gets.strip.to_i 
+    show_events_for(chosen_category) if valid_input(chosen_category, @categories)
+  end
 
-    def valid_input(input, data)
-      input.to_i <= data.length && input.to_i > 0
-    end
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0
+  end
+
+  def show_events_for(chosen_category)
+    category = @categories[chosen_category - 1]
+    puts "Here are classes for #{category}"
+    # To implement
+    # EdenGardenClasses::Event.all.each.with_index(1) do | event |
+    #   puts event.name
+    # end
+    # get_user_event
+  end
 end
